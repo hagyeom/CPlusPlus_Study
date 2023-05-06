99393da502e2188b2e6c1a42fe986e3a300da409
하겸 윤 <younha3710@naver.com>
2023-05-06 22:23:42 +0900

    ㅇㅇ

---
+++ Project2/ch05_test02.cpp
@@ -0,0 +1,14 @@
+// 명품 C++ 프로그래밍 5장 실습문제 2번
+#include <iostream>
+using namespace std;
+
+double half(double &n) {
+	n /= 2;
+	return n;
+}
+
+int main() {
+	double n = 20;
+	half(n); // n의 반값을 구해 n을 바꾼다.
+	cout << n; // 10이 출력된다.
+}
