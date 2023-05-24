17473888d52bc8c28884419e9eaf4e65fa94feed
hagyeom <116890147+hagyeom@users.noreply.github.com>
2023-05-24 12:25:08 +0900

    Add files via upload

---
+++ ch07_test09.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Circle {
+	int radius;
+public:
+	Circle(int radius = 0) { this->radius = radius; }
+	void show() { cout << "radius = " << radius << " 인 원" << endl; }
+	friend Circle operator+(int op1, Circle op2);
+};
+
+Circle operator+(int op1, Circle op2) {
+	Circle tmp;
+	tmp.radius = op1 + op2.radius;
+	return tmp;
+}
+int main() {
+	Circle a(5), b(4);
+	b = 1 + a; // b의 반지름을 a의 반지름에 1을 더한 것으로 변경
+	a.show();
+	b.show();
+}
