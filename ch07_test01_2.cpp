f3b79ee37a75da5a8c79fd350cd13e193255dbd5
hagyeom <116890147+hagyeom@users.noreply.github.com>
2023-05-24 12:08:45 +0900

    Add files via upload

---
+++ ch07_test01_2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Book {
+	string title;
+	int price, pages;
+public:
+	Book(string title = "", int price = 0, int pages = 0) {
+		this->title = title; this->price = price; this->pages = pages;
+	}
+	void show() {
+		cout << title << ' ' << price << "원 " << pages << " 페이지" << endl;
+	}
+	string getTitle() { return title; }
+	friend Book& operator+=(Book& b, int price);
+	friend Book& operator-=(Book& b, int price);
+};
+
+Book& operator+=(Book& b, int price) {
+	b.price += price;
+	return b;
+}
+
+Book& operator-=(Book& b, int price) {
+	b.price -= price;
+	return b;
+}
+int main() {
+	Book a("청춘", 20000, 300), b("미래", 30000, 500);
+	a += 500; // 책 a의 가격 500원 증가
+	b -= 500; // 책 b의 가격 500원 감소
+	a.show();
+	b.show();
+}
